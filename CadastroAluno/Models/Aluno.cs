8bc3d4b357f8d0625ed33866250b360b8784ac34
GUILHERME SANCHES DA ROCHA <guilherme.srocha11@senacsp.edu.br>
2022-09-29 22:05:33 -0300

    Criando Aluno (Model,Controller,DbContext,Migration)

---
+++ CadastroAluno/Models/Aluno.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroAluno.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Turma { get; set; }
+        public double Media { get; set; }
+
+        public void AtualizarDados(string nome, string turma)
+        {
+            Nome = nome;
+            Turma = turma;
+
+        }
+
+        public bool VerificaAprovação() => Media > 5;
+
+
+        public void AtualizaMedia(double novaMedia)
+        {
+            Media = novaMedia;
+        }
+    }
+}
